ce04b06f1b864df1ab1a3b0179b6048682cbd287
tolumide-ng <tolumideshopein@gmail.com>
2024-11-30 05:44:10 +0100

    Returns whether a packet_type should have a packet_identifier or not

--- hivemqtt-core/src/traits/variable_header.rs
+++ hivemqtt-core/src/traits/variable_header.rs
@@ -1,8 +1,16 @@
-use crate::commons::packet_type::PacketType;
+use crate::commons::{fixed_header::FixedHeaderFlag, packet_type::PacketType, qos::QoS};
 
 /// Variable Header (Present in some MQTT Control Packets)
 /// Last update: Chapter 2.1
 pub(crate) trait VariableHeader {
     /// Optional: Two Byte Integer Packet Identifier field
-    fn packet_identifier(packet: PacketType) {}
+    fn has_packet_identifier(packet: PacketType) -> bool {
+        match packet {
+            PacketType::Publish(value) => FixedHeaderFlag::from(value).qos > QoS::Zero,
+            PacketType::PubAck | PacketType::PubRec | PacketType::PubRel | 
+                PacketType::PubComp | PacketType::Subscribe | PacketType::SubAck | 
+                PacketType::UnSubscribe | PacketType::UnSubAck => true,
+            _ => false
+        }
+    }
 }
