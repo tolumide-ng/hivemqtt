816cac75e8f1ca453aacb4296e4ab7cc55a41c52
tolumide-ng <tolumideshopein@gmail.com>
2024-12-03 04:36:51 +0100

    Creates connack mod

--- hivemqtt-core/src/packet/connack/mod.rs
+++ hivemqtt-core/src/packet/connack/mod.rs
@@ -1,1 +1,19 @@
+use bytes::BufMut;
+
+use crate::{commons::{packets::Packet, variable_byte_integer::variable_integer}, traits::write::ControlPacket};
+
 pub(crate) struct Connack {}
+
+
+impl ControlPacket for Connack {
+    /// In this case:
+    /// This is the length of the Variable Header
+    fn length(&self) -> usize {
+        0
+    }
+
+    fn w(&self, buf: &mut bytes::BytesMut) {
+        buf.put_u8(u8::from(Packet::ConnAck));
+        let _ = variable_integer(buf, self.length()); // Variable Header encoded as Variable Byte Integer
+    }
+}
--- hivemqtt-core/src/traits/write.rs
+++ hivemqtt-core/src/traits/write.rs
@@ -4,14 +4,10 @@ pub(crate) trait ControlPacket {
     fn w(&self, buff: &mut BytesMut);
 
     /// Writes the length of the bytes and itself into the buffer
-    fn ws(&self, buff: &mut BytesMut, value: &[u8]) {
-        buff.put_u16(value.len() as u16);
-        buff.extend_from_slice(value);
+    fn ws(&self, buf: &mut BytesMut, value: &[u8]) {
+        buf.put_u16(value.len() as u16);
+        buf.extend_from_slice(value);
     }
-
-
-    /// Writes the vairable header into the buffer (write_variable_header)
-    fn w_vh(&self, buff: &mut BytesMut) {}
 
     /// Allows a struct specify what it's length is to it's external users
     /// Normally this is obtainable using the .len() method (internally on structs implementing DataSize),
