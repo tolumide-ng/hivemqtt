f26e7472168c32244459e289888c6533c548ec4d
tolumide-ng <tolumideshopein@gmail.com>
2024-12-04 19:10:57 +0100

    Enables connack write

--- hivemqtt-core/src/packet/connack/properties.rs
+++ hivemqtt-core/src/packet/connack/properties.rs
@@ -1,7 +1,11 @@
+use std::borrow::{Borrow, Cow};
+
 use bytes::Bytes;
 use hivemqtt_macros::DataSize;
 
-use super::ControlPacket;
+use crate::commons::property::Property;
+
+use super::{variable_integer, ControlPacket};
 
 #[derive(Debug, DataSize)]
 pub(crate) struct Properties {
@@ -37,21 +41,35 @@ pub(crate) struct Properties {
     server_reference: Option<String>,
     #[bytes[wl_2]]
     authentication_method: Option<String>,
-    
+    #[bytes[wl_2]]
     authentication_data: Option<Bytes>
 }
-
-
-// The server uses this value to give additional information to the Client. 
-//      
 
 impl ControlPacket for Properties {
     /// Length of the properties in the CONNACK packet Variable Header encoded as Variable Byte Integer
     fn length(&self) -> usize {
-        0
+        self.len()
     }
 
     fn w(&self, buff: &mut bytes::BytesMut) {
-        
+        let _ = variable_integer(buff, self.length());
+
+        Property::SessionExpiryInterval(self.session_expiry_interval).w(buff);
+        Property::ReceiveMaximum(self.receive_maximum).w(buff);
+        Property::MaximumQoS(self.maximum_qos.map(|q| q as u8)).w(buff);
+        Property::RetainAvailable(self.retain_available.map(|x| x as u8)).w(buff);
+        Property::MaximumPacketSize(self.maximum_packet_size).w(buff);
+        Property::AssignedClientIdentifier(self.assigned_client_id.as_deref().map(Cow::Borrowed)).w(buff);
+        Property::TopicAliasMaximum(self.topic_alias_maximum).w(buff);
+        Property::ReasonString(self.reason_string.borrow().as_deref().map(Cow::Borrowed)).w(buff);
+        Property::UserProperty(Cow::Borrowed(&self.user_property)).w(buff);
+        Property::WildCardSubscription(self.wildcard_subscription_available.map(|x| x as u8)).w(buff);
+        Property::SubscriptionIdentifierAvailable(self.session_expiry_interval.map(|x| x as u8)).w(buff);
+        Property::SharedSubscriptionAvailable(self.shared_subscription_available.map(|x| x as u8)).w(buff);
+        Property::ServerKeepAlive(self.server_keep_alive).w(buff);
+        Property::ResponseInformation(self.response_information.as_deref().map(Cow::Borrowed)).w(buff);
+        Property::ServerReference(self.server_reference.as_deref().map(Cow::Borrowed)).w(buff);
+        Property::AuthenticationMethod(self.authentication_method.as_deref().map(Cow::Borrowed)).w(buff);
+        Property::AuthenticationData(self.authentication_data.as_deref().map(Cow::Borrowed)).w(buff);
     }
 }
