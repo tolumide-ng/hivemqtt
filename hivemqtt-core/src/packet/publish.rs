95e793725bd2b75159096e94c4f1167d1e3dff0f
tolumide-ng <tolumideshopein@gmail.com>
2024-12-05 17:32:16 +0100

    Adds some extra methods to the publish packet

--- hivemqtt-core/src/packet/publish.rs
+++ hivemqtt-core/src/packet/publish.rs
@@ -1,21 +1,48 @@
 use bytes::BufMut;
 use hivemqtt_macros::Length;
 
-use crate::{commons::{packets::Packet, qos::QoS}, traits::write::ControlPacket};
+use crate::{commons::{packets::Packet, property::Property, qos::QoS}, traits::write::ControlPacket};
 
 #[derive(Debug, Length)]
 pub(crate) struct Publish {
     dup: bool,
     retain: bool,
     qos: QoS,
+    topic: String,
+    packet_identifier: u16,
+    #[bytes(ignore)]
+    properties: PublishProperties,
 }
 
 impl ControlPacket for Publish {
     fn length(&self) -> usize {
+        // (variable header + length of the payload), encoded as Variable Byte Integer
         0
     }
 
     fn w(&self, buf: &mut bytes::BytesMut) {
         buf.put_u8(u8::from(Packet::Publish) | (self.dup as u8) << 3 | (self.qos as u8) << 1 | (self.retain as u8));
+        // this part below needs to be reconfirmed
+        let _ = self.encode_variable_length(buf, self.length()); // not sure yet.
+        self.ws(buf, self.topic.as_bytes());
+
+        if self.qos !=QoS::Zero {buf.put_u16(self.packet_identifier)}
+        let _ = self.encode_variable_length(buf, self.properties.length());
+    }
+}
+
+
+#[derive(Debug, Length)]
+pub(crate) struct PublishProperties {
+    payload_format_indicator: Option<u8>,
+}
+
+impl ControlPacket for PublishProperties {
+    fn length(&self) -> usize {
+        self.len()
+    }
+    
+    fn w(&self, buf: &mut bytes::BytesMut) {
+        Property::PayloadFormatIndicator(self.payload_format_indicator).w(buf);
     }
 }
--- hivemqtt-core/src/traits/write.rs
+++ hivemqtt-core/src/traits/write.rs
@@ -1,7 +1,41 @@
 use bytes::{BufMut, BytesMut};
 
+use crate::commons::error::MQTTError;
+
 pub(crate) trait ControlPacket {
     fn w(&self, buf: &mut BytesMut);
+
+    fn get_variable_length(len: usize) -> usize {
+        if len >= 2_097_152 { 4 }
+        else if len >= 16_384 { 3 }
+        else if len >= 128 { 2 }
+        else { 1 }
+    }
+
+    fn encode_variable_length(&self, buf: &mut BytesMut, len: usize) -> Result<usize, MQTTError> {
+         // 268_435_455
+        if len > 0xFFFFFFF {
+            return Err(MQTTError::PayloadTooLong)
+        }
+        let mut x: usize = len;
+        let mut count = 0;
+
+        while x != 0 {
+            // Get the last 7bits of x;
+            let mut bytes = (x & 0x7F) as u8; // (x%128)
+            x >>= 7; // shift to right by 7bits(i.e. 2^7 = 128) (i.e x = x/128)
+
+            if x > 0 {
+                bytes |= 0x80
+            }
+            buf.put_u8(bytes);
+            count += 1;
+        }
+
+        Ok(count)
+    }
+
+    fn decode_variable_length(&self, buf: &mut BytesMut) {}
 
     /// Writes the length of the bytes and itself into the buffer
     fn ws(&self, buf: &mut BytesMut, value: &[u8]) {
