139e087aef08cecebad2f344dd7fa92c458c3722
tolumide-ng <tolumideshopein@gmail.com>
2024-12-18 02:06:13 +0100

    Adds tests on the auth packet

--- hivemqtt-core/src/packet/auth/mod.rs
+++ hivemqtt-core/src/packet/auth/mod.rs
@@ -3,7 +3,7 @@ mod reason_code;
 
 pub use properties::{AuthProperties, AuthReasonCode};
 
-use bytes::{BufMut, Bytes};
+use bytes::Bytes;
 
 use crate::{commons::{error::MQTTError, fixed_header::FixedHeader, packets::Packet, property::Property}, traits::{bufferio::BufferIO, read::Read, write::Write}};
 
@@ -30,6 +30,10 @@ impl BufferIO for Auth {
 
     fn read(buf: &mut Bytes) -> Result<Self, MQTTError> {
         let mut packet = Self::default();
+
+        // reason code and property length can be omitted if reason_code is success and there are no properties
+        if buf.is_empty() { return Ok(packet) }
+
         packet.reason_code = AuthReasonCode::try_from(u8::read(buf)?).map_err(MQTTError::UnknownData)?;
         packet.properties = AuthProperties::read(buf)?;
 
@@ -42,10 +46,42 @@ impl BufferIO for Auth {
 
 #[cfg(test)]
 mod tests {
+    use bytes::BytesMut;
+
     use super::*;
 
     #[test]
     fn read_write_when_reason_code_code_and_property_length_is_omitted() {
         // reason code and property length can be omitted if the reason code is 0x00(Success) and there are no properties
+        let packet = Auth::default();
+        let mut buf = BytesMut::with_capacity(10);
+        packet.write(&mut buf).unwrap();
+
+        assert_eq!(buf, b"\xf0\x02\0\0".to_vec());
+
+        let mut read_buf = Bytes::from_iter(buf.to_vec());
+        let fixed_header = FixedHeader::read(&mut read_buf).unwrap();
+
+        assert_eq!(fixed_header.flags, 0);
+        assert_eq!(fixed_header.remaining_length, 2);
+        assert_eq!(fixed_header.packet_type, Packet::Auth);
+
+        let read_packet = Auth::read(&mut read_buf).unwrap();
+        assert_eq!(read_packet.reason_code, AuthReasonCode::Success);
+        assert_eq!(packet, read_packet);
+    }
+
+    #[test]
+    fn reason_code_and_property_length_are_omitted() {
+        let mut buf = Bytes::from_iter(b"\xf0\0".to_vec());
+        let fixed_header = FixedHeader::read(&mut buf).unwrap();
+
+        assert_eq!(fixed_header.flags, 0);
+        assert_eq!(fixed_header.remaining_length, 0);
+        assert_eq!(fixed_header.packet_type, Packet::Auth);
+
+        let packet = Auth::read(&mut buf).unwrap();
+
+        assert_eq!(packet, Auth::default());
     }
 }
