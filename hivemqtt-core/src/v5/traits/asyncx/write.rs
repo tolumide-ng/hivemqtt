93d269af1e2ee2c5f32a5750af505db915c051e1
tolumide-ng <tolumideshopein@gmail.com>
2025-01-09 22:37:50 +0100

    Implements AsyncWrite on most of the used types

--- hivemqtt-core/src/v5/traits/asyncx/write.rs
+++ hivemqtt-core/src/v5/traits/asyncx/write.rs
@@ -1,7 +1,52 @@
 use std::future::Future;
 
+use bytes::Bytes;
+use futures::AsyncWriteExt;
+
 use crate::v5::commons::error::MQTTError;
 
 pub(crate) trait AsyncWrite<S>: Sized {
-    fn writex() -> impl Future<Output = Result<(), MQTTError>>;
+    fn write(&self, stream: &mut S) -> impl Future<Output = Result<(), MQTTError>>;
+}
+
+impl<S> AsyncWrite<S> for u8
+    where S: AsyncWriteExt + Unpin {
+        async fn write(&self, stream: &mut S) -> Result<(), MQTTError> {
+            stream.write_all(&self.to_be_bytes()).await?;
+            Ok(())
+        }
+}
+
+impl<S> AsyncWrite<S> for u16
+    where S: AsyncWriteExt + Unpin {
+        async fn write(&self, stream: &mut S) -> Result<(), MQTTError> {
+            stream.write_all(&self.to_be_bytes()).await?;
+            Ok(())
+        }
+}
+
+impl<S> AsyncWrite<S> for u32
+    where S: AsyncWriteExt + Unpin {
+        async fn write(&self, stream: &mut S) -> Result<(), MQTTError> {
+            stream.write_all(&self.to_be_bytes()).await?;
+            Ok(())
+        }
+}
+
+impl<S> AsyncWrite<S> for Bytes
+    where S: AsyncWriteExt + Unpin {
+        async fn write(&self, stream: &mut S) -> Result<(), MQTTError> {
+            stream.write_all(&(self.len() as u16).to_be_bytes()).await?;
+            stream.write_all(&self).await?;
+            Ok(())
+        }
+}
+
+impl<S> AsyncWrite<S> for String
+    where S: AsyncWriteExt + Unpin {
+        async fn write(&self, stream: &mut S) -> Result<(), MQTTError> {
+            stream.write_all(&(self.len() as u16).to_be_bytes()).await?;
+            stream.write_all(&self.as_bytes()).await?;
+            Ok(())
+        }
 }
