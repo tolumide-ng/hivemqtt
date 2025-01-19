0368dc93a2b1779a3b3f8acc3df42da4e86f3f01
tolumide-ng <tolumideshopein@gmail.com>
2025-01-19 02:11:58 +0100

    Implements Update on Disconnect packet

--- hivemqtt-core/src/v5/packet/disconnect/mod.rs
+++ hivemqtt-core/src/v5/packet/disconnect/mod.rs
@@ -1,12 +1,16 @@
-mod reason_code;
 mod properties;
+mod reason_code;
 
 use bytes::Buf;
 pub use properties::DisconnectProperties;
 pub use reason_code::DisconnectReasonCode;
 
-
-use crate::v5::{commons::{error::MQTTError, fixed_header::FixedHeader, packet_type::PacketType, property::Property}, traits::{syncx::bufferio::BufferIO, syncx::read::Read, syncx::write::Write}};
+use crate::v5::{
+    commons::{
+        error::MQTTError, fixed_header::FixedHeader, packet_type::PacketType, property::Property,
+    },
+    traits::{syncx::bufferio::BufferIO, syncx::read::Read, syncx::write::Write},
+};
 
 #[derive(Debug, Default, PartialEq, Eq)]
 pub struct Disconnect {
@@ -17,11 +21,14 @@ pub struct Disconnect {
 impl BufferIO for Disconnect {
     // the length of the dsiconnect variable header
     fn length(&self) -> usize {
-         // The Reason Code and Property Length can be omitted if the Reason Code is 0x00 (Normal disconnecton) and there are no Properties. In this case the DISCONNECT has a Remaining Length of 0.
-        if self.reason_code == DisconnectReasonCode::NormalDisconnection && self.properties.length() == 0 {
+        // The Reason Code and Property Length can be omitted if the Reason Code is 0x00 (Normal disconnecton) and there are no Properties. In this case the DISCONNECT has a Remaining Length of 0.
+        if self.reason_code == DisconnectReasonCode::NormalDisconnection
+            && self.properties.length() == 0
+        {
             return 0;
         }
-        return self.properties.length() + self.properties.variable_length() + 1 // 1 is for the reason code abovegit stat
+        return self.properties.length() + self.properties.variable_length() + 1;
+        // 1 is for the reason code abovegit stat
     }
 
     fn write(&self, buf: &mut bytes::BytesMut) -> Result<(), MQTTError> {
@@ -29,7 +36,9 @@ impl BufferIO for Disconnect {
 
         u8::from(self.reason_code).write(buf);
 
-        if self.reason_code == DisconnectReasonCode::NormalDisconnection && self.properties.length() == 0 {
+        if self.reason_code == DisconnectReasonCode::NormalDisconnection
+            && self.properties.length() == 0
+        {
             return Ok(());
         }
 
@@ -39,17 +48,132 @@ impl BufferIO for Disconnect {
 
     fn read(buf: &mut bytes::Bytes) -> Result<Self, MQTTError> {
         let mut packet = Self::default();
-        packet.reason_code = DisconnectReasonCode::try_from(u8::read(buf)?).map_err(MQTTError::UnknownData)?;
-        
+        packet.reason_code =
+            DisconnectReasonCode::try_from(u8::read(buf)?).map_err(MQTTError::UnknownData)?;
+
         if buf.has_remaining() {
             packet.properties = DisconnectProperties::read(buf)?;
         }
 
         Ok(packet)
     }
-
 }
 
+mod syncx {
+    use bytes::{Buf, Bytes, BytesMut};
+
+    use crate::v5::{
+        commons::error::MQTTError,
+        traits::{
+            bufferio::BufferIO,
+            syncx::{read::Read, write::Write},
+        },
+    };
+
+    use super::{Disconnect, DisconnectProperties, DisconnectReasonCode, FixedHeader, PacketType};
+
+    impl BufferIO for Disconnect {
+        // the length of the dsiconnect variable header
+        fn length(&self) -> usize {
+            // The Reason Code and Property Length can be omitted if the Reason Code is 0x00 (Normal disconnecton) and there are no Properties. In this case the DISCONNECT has a Remaining Length of 0.
+            if self.reason_code == DisconnectReasonCode::NormalDisconnection
+                && self.properties.length() == 0
+            {
+                return 0;
+            }
+            return self.properties.length() + self.properties.variable_length() + 1;
+            // 1 is for the reason code abovegit stat
+        }
+
+        fn write(&self, buf: &mut BytesMut) -> Result<(), MQTTError> {
+            FixedHeader::new(PacketType::Disconnect, 0, self.length()).write(buf)?;
+
+            u8::from(self.reason_code).write(buf);
+
+            if self.reason_code == DisconnectReasonCode::NormalDisconnection
+                && self.properties.length() == 0
+            {
+                return Ok(());
+            }
+
+            self.properties.write(buf)?;
+            Ok(())
+        }
+
+        fn read(buf: &mut Bytes) -> Result<Self, MQTTError> {
+            let mut packet = Self::default();
+            packet.reason_code =
+                DisconnectReasonCode::try_from(u8::read(buf)?).map_err(MQTTError::UnknownData)?;
+
+            if buf.has_remaining() {
+                packet.properties = DisconnectProperties::read(buf)?;
+            }
+
+            Ok(packet)
+        }
+    }
+}
+
+mod asyncx {
+
+    use crate::v5::{
+        commons::error::MQTTError,
+        traits::{
+            asyncx::{read::Read, write::Write},
+            streamio::StreamIO,
+        },
+    };
+
+    use super::{Disconnect, DisconnectProperties, DisconnectReasonCode, FixedHeader, PacketType};
+
+    impl StreamIO for Disconnect {
+        // the length of the dsiconnect variable header
+        fn length(&self) -> usize {
+            // The Reason Code and Property Length can be omitted if the Reason Code is 0x00 (Normal disconnecton) and there are no Properties. In this case the DISCONNECT has a Remaining Length of 0.
+            if self.reason_code == DisconnectReasonCode::NormalDisconnection
+                && self.properties.length() == 0
+            {
+                return 0;
+            }
+            return self.properties.length() + self.properties.variable_length() + 1;
+            // 1 is for the reason code abovegit stat
+        }
+
+        async fn write<W>(&self, stream: &mut W) -> Result<(), crate::v5::commons::error::MQTTError>
+        where
+            W: futures::AsyncWriteExt + Unpin,
+        {
+            FixedHeader::new(PacketType::Disconnect, 0, self.length())
+                .write(stream)
+                .await?;
+
+            u8::from(self.reason_code).write(stream).await;
+
+            if self.reason_code == DisconnectReasonCode::NormalDisconnection
+                && self.properties.length() == 0
+            {
+                return Ok(());
+            }
+
+            self.properties.write(stream).await?;
+            Ok(())
+        }
+
+        async fn read<R>(stream: &mut R) -> Result<Self, crate::v5::commons::error::MQTTError>
+        where
+            R: futures::AsyncReadExt + Unpin,
+        {
+            let mut packet = Self::default();
+
+            packet.reason_code = DisconnectReasonCode::try_from(u8::read(stream).await?)
+                .map_err(MQTTError::UnknownData)?;
+
+            packet.properties = DisconnectProperties::read(stream).await?;
+
+            Ok(packet)
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -73,7 +197,10 @@ mod tests {
         assert_eq!(fixed_header.packet_type, PacketType::Disconnect);
 
         let read_packet = Disconnect::read(&mut read_buf).unwrap();
-        assert_eq!(read_packet.reason_code, DisconnectReasonCode::NormalDisconnection);
+        assert_eq!(
+            read_packet.reason_code,
+            DisconnectReasonCode::NormalDisconnection
+        );
     }
 
     #[test]
@@ -92,9 +219,12 @@ mod tests {
         assert_eq!(fixed_header.flags, None);
         assert_eq!(fixed_header.remaining_length, 2);
         assert_eq!(fixed_header.packet_type, PacketType::Disconnect);
-        
+
         let read_packet = Disconnect::read(&mut read_buf).unwrap();
-        assert_eq!(read_packet.reason_code, DisconnectReasonCode::MaximumConnectTime);
+        assert_eq!(
+            read_packet.reason_code,
+            DisconnectReasonCode::MaximumConnectTime
+        );
         assert_eq!(packet, read_packet);
     }
 
@@ -102,13 +232,19 @@ mod tests {
     fn read_write_disconnect_packet_with_properties() {
         let mut packet = Disconnect::default();
         packet.reason_code = DisconnectReasonCode::MaximumConnectTime;
-        packet.properties = DisconnectProperties {session_expiry_interval: Some(0x3A), 
-            reason_string: Some("aVery good string3898 &**".into()), user_property: vec![], server_reference: Some("mqtt5.0.dev".into())};
+        packet.properties = DisconnectProperties {
+            session_expiry_interval: Some(0x3A),
+            reason_string: Some("aVery good string3898 &**".into()),
+            user_property: vec![],
+            server_reference: Some("mqtt5.0.dev".into()),
+        };
 
         let mut buf = BytesMut::with_capacity(10);
         packet.write(&mut buf).unwrap();
 
-        let expected = b"\xe01\xa0/\x11\0\0\0:\x1f\0\x19aVery good string3898 &**\x1c\0\x0bmqtt5.0.dev".to_vec();
+        let expected =
+            b"\xe01\xa0/\x11\0\0\0:\x1f\0\x19aVery good string3898 &**\x1c\0\x0bmqtt5.0.dev"
+                .to_vec();
 
         assert_eq!(buf, expected);
 
@@ -118,9 +254,12 @@ mod tests {
         assert_eq!(fixed_header.flags, None);
         assert_eq!(fixed_header.remaining_length, 49);
         assert_eq!(fixed_header.packet_type, PacketType::Disconnect);
-        
+
         let read_packet = Disconnect::read(&mut read_buf).unwrap();
-        assert_eq!(read_packet.reason_code, DisconnectReasonCode::MaximumConnectTime);
+        assert_eq!(
+            read_packet.reason_code,
+            DisconnectReasonCode::MaximumConnectTime
+        );
         assert_eq!(packet, read_packet);
     }
 }
--- hivemqtt-core/src/v5/packet/disconnect/properties.rs
+++ hivemqtt-core/src/v5/packet/disconnect/properties.rs
@@ -1,11 +1,11 @@
 use std::borrow::Cow;
 
+use bytes::Bytes;
 use hivemqtt_macros::Length;
 
 use crate::v5::commons::error::MQTTError;
 
 use super::{BufferIO, Property};
-
 
 #[derive(Debug, Length, Default, PartialEq, Eq)]
 pub struct DisconnectProperties {
@@ -16,36 +16,158 @@ pub struct DisconnectProperties {
 }
 
 impl BufferIO for DisconnectProperties {
-    fn length(&self) -> usize { self.len() }
+    fn length(&self) -> usize {
+        self.len()
+    }
 
     fn write(&self, buf: &mut bytes::BytesMut) -> Result<(), MQTTError> {
         self.encode(buf)?;
         Property::SessionExpiryInterval(self.session_expiry_interval).w(buf);
         Property::ReasonString(self.reason_string.as_deref().map(Cow::Borrowed)).w(buf);
-        self.user_property.iter().for_each(|up| Property::UserProperty(Cow::Borrowed(&up)).w(buf));
+        self.user_property
+            .iter()
+            .for_each(|up| Property::UserProperty(Cow::Borrowed(&up)).w(buf));
         Property::ServerReference(self.server_reference.as_deref().map(Cow::Borrowed)).w(buf);
         Ok(())
     }
 
-
     fn read(buf: &mut bytes::Bytes) -> Result<Self, MQTTError> {
-        let Some(len) = Self::parse_len(buf)? else { return Ok(Self::default()) };
-        let mut props = Self::default();
+        let Some(len) = Self::parse_len(buf)? else {
+            return Ok(Self::default());
+        };
+
         let mut data = buf.split_to(len);
 
+        Self::read_data(&mut data)
+    }
+}
+
+impl DisconnectProperties {
+    fn read_data(data: &mut Bytes) -> Result<Self, MQTTError> {
+        let mut props = Self::default();
+
         loop {
-            let property = Property::read(&mut data)?;
+            let property = Property::read(data)?;
             match property {
-                Property::ReasonString(ref v) => Self::try_update(&mut props.reason_string, v.as_deref().map(String::from))(property)?,
+                Property::ReasonString(ref v) => Self::try_update(
+                    &mut props.reason_string,
+                    v.as_deref().map(String::from),
+                )(property)?,
                 Property::UserProperty(v) => props.user_property.push(v.into_owned()),
-                Property::SessionExpiryInterval(v) => Self::try_update(&mut props.session_expiry_interval, v)(property)?,
-                Property::ServerReference(ref v) => Self::try_update(&mut props.server_reference, v.as_deref().map(String::from))(property)?,
-                p => return Err(MQTTError::UnexpectedProperty(p.to_string(), "".to_string()))
+                Property::SessionExpiryInterval(v) => {
+                    Self::try_update(&mut props.session_expiry_interval, v)(property)?
+                }
+                Property::ServerReference(ref v) => Self::try_update(
+                    &mut props.server_reference,
+                    v.as_deref().map(String::from),
+                )(property)?,
+                p => return Err(MQTTError::UnexpectedProperty(p.to_string(), "".to_string())),
             }
 
-            if data.is_empty() { break; }
+            if data.is_empty() {
+                break;
+            }
         }
 
         Ok(props)
+    }
+}
+
+mod syncx {
+    use std::borrow::Cow;
+
+    use crate::v5::{
+        commons::{error::MQTTError, property::new_approach::Property},
+        traits::bufferio::BufferIO,
+    };
+
+    use super::DisconnectProperties;
+
+    impl BufferIO for DisconnectProperties {
+        fn length(&self) -> usize {
+            self.len()
+        }
+
+        fn write(&self, buf: &mut bytes::BytesMut) -> Result<(), MQTTError> {
+            self.encode(buf)?;
+
+            Property::SessionExpiryInterval(self.session_expiry_interval).write(buf)?;
+            Property::ReasonString(self.reason_string.as_deref().map(Cow::Borrowed)).write(buf)?;
+            self.user_property
+                .iter()
+                .try_for_each(|up| Property::UserProperty(Cow::Borrowed(&up)).write(buf))?;
+            Property::ServerReference(self.server_reference.as_deref().map(Cow::Borrowed))
+                .write(buf)?;
+
+            Ok(())
+        }
+
+        fn read(buf: &mut bytes::Bytes) -> Result<Self, MQTTError> {
+            let Some(len) = Self::parse_len(buf)? else {
+                return Ok(Self::default());
+            };
+
+            let mut data = buf.split_to(len);
+
+            Self::read_data(&mut data)
+        }
+    }
+}
+
+mod asyncx {
+    use std::borrow::Cow;
+
+    use bytes::Bytes;
+
+    use crate::v5::{commons::property::new_approach::Property, traits::streamio::StreamIO};
+
+    use super::DisconnectProperties;
+
+    impl StreamIO for DisconnectProperties {
+        fn length(&self) -> usize {
+            self.len()
+        }
+
+        async fn write<W>(&self, stream: &mut W) -> Result<(), crate::v5::commons::error::MQTTError>
+        where
+            W: futures::AsyncWriteExt + Unpin,
+        {
+            self.encode(stream).await?;
+
+            Property::SessionExpiryInterval(self.session_expiry_interval)
+                .write(stream)
+                .await?;
+
+            Property::ReasonString(self.reason_string.as_deref().map(Cow::Borrowed))
+                .write(stream)
+                .await?;
+
+            for kv in &self.user_property {
+                Property::UserProperty(Cow::Borrowed(&kv))
+                    .write(stream)
+                    .await?
+            }
+
+            Property::ServerReference(self.server_reference.as_deref().map(Cow::Borrowed))
+                .write(stream)
+                .await?;
+
+            Ok(())
+        }
+
+        async fn read<R>(stream: &mut R) -> Result<Self, crate::v5::commons::error::MQTTError>
+        where
+            R: futures::AsyncReadExt + Unpin,
+        {
+            let Some(len) = Self::parse_len(stream).await? else {
+                return Ok(Self::default());
+            };
+
+            let mut data = Vec::with_capacity(len);
+            stream.read_exact(&mut data).await?;
+            let mut data = Bytes::copy_from_slice(&data);
+
+            Self::read_data(&mut data)
+        }
     }
 }
